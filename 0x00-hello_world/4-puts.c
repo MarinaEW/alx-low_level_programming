a812ba0ea2f4bccac4b8062a78c26058043df553
MarinaEW <marina.ehab016@gmail.com>
2023-03-16 13:50:18 -0700

    5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
-/**
- * This is task 4
- *
- * return 0 means no error
- */
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle
 	return (0);
 }
