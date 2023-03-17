1849585c040b6ac85a6c0a114221fa36566a9375
MarinaEW <marina.ehab016@gmail.com>
2023-03-17 13:09:56 -0700

    7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main- Entry point
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+char ch;
+for (ch = 'z' ; ch >= 'a' ; ch--)
+putchar(ch);
+putchar('\n');
+return (0);
+}
