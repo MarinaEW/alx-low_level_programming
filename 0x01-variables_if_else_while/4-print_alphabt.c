f2f5cd386fdec28ca384aaad69fae1fb93e25a58
MarinaEW <marina.ehab016@gmail.com>
2023-03-17 12:42:33 -0700

    4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main- Entry point
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+if (ch != 'q' && ch != 'e')
+putchar(ch);
+putchar('\n');
+return (0);
+}
