1b9b162860ad2577631e45766817b8a7881306f8
MarinaEW <marina.ehab016@gmail.com>
2023-03-21 19:04:57 -0700

    5-6

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * print_sign - function that prints signs
+ *
+ * @n: parameter to be checked
+ * Return: 0 if no. is 0
+ * and 1 otherwise
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+_putchar(',');
+_putchar(' ');
+return (1);
+}
+else if (n == 0)
+{
+_putchar('0');
+_putchar(',');
+_putchar(' ');
+return (0);
+}
+else
+{
+_putchar('-');
+_putchar(',');
+_putchar(' ');
+return (-1);
+}
+}
