2f1fa4f49ba9fe8ae593832216c85b972199e6c4
MarinaEW <marina.ehab016@gmail.com>
2023-03-21 20:00:03 -0700

    10-11

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - adds two numbers
+ *
+ * @a: no. to be added
+ * @b: no. to be added
+ * return: sum
+ */
+int add(int a, int b)
+{
+return (a + b);
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ *
+ */
+void print_to_98(int n)
+{
+int i;
+if (n >= 0 &7 n <=98)
+{
+for (i = n ;i <= 98; i++)
+{
+printf("%d, ", i)
+}
+printf('\n');
+else if ( < 0)
+{
