aa6dc7fc05a8696b72df19a55380fa0a1b01be07
MarinaEW <marina.ehab016@gmail.com>
2023-03-21 17:29:27 -0700

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,21 @@
-#include <stdio.h>
+#include "main.h"
 /**
- * main- Entry point
+ * putchar: print alphabets of putchar
+ *
+ *main - Entry point
  *
  * Return: always 0 (success)
  */
 int main(void)
 {
-printf("_putchar\n");
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }
