0e20ffb1eec2c333effb18ba8e339884c3909081
MarinaEW <marina.ehab016@gmail.com>
2023-04-05 18:00:23 -0700

    1-2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen_recursion - fn. that returns the length of a string.
+ * @s: string
+ * Return: length of string
+ */
+int _strlen_recursion(char *s)
+{
+int len = 0;
+if (*s > '\0')
+{
+len = _strlen_recursion(s + 1) + 1;
+}
+return (len);
+}
