1058c88dd86db0147c91a0232155cd318a4a09e8
MarinaEW <marina.ehab016@gmail.com>
2023-04-20 18:17:05 -0700

    1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -10,11 +10,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 va_list list;
 unsigned int i;
 va_start(list , n);
-if (!n)
-{
-printf("\n");
-return;
-}
 for (i = 0 ; i < n ; i++)
 {
 printf("%d", va_arg(list, int));
