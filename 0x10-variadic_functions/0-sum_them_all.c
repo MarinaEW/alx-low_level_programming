db451a38e321fb06253de3413c06182739f1560d
MarinaEW <marina.ehab016@gmail.com>
2023-04-20 18:07:25 -0700

    0-1

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,22 @@
+#include "variadic_functions.h"
+/**
+ * sum_them_all - sums var. arguements
+ * @n: no. of arguements
+ * @...: integers to be summed
+ * Return: sum
+ *
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+va_list list;
+unsigned int i;
+int sum = 0;
+if (n == 0)
+return (0);
+va_start(list, n);
+sum = 0;
+for (i = 0; i < n; i++)
+sum = sum + va_arg(list , int);
+va_end(list);
+return (sum);
+}
---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+ * print_numbers - fn. that prints numbers, followed by a new line.
+ * @separator: string to be printed between numbers
+ * @n: number of integers passed to the function
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+va_list list;
+unsigned int i;
+va_start(list , n);
+if (n <= 0)
+{
+printf("\n");
+return;
+}
+for (i = 0 ; i < n ; i++)
+{
+printf("%d", va_arg(list, int));
+if (i != (n - 1) && separator != NULL)
+printf("%s", separator);
+}
+printf("\n");
+va_end(list);
+}
