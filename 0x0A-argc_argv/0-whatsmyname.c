9e97200cf87b387c8f1dd716597f569b1baa3c36
MarinaEW <marina.ehab016@gmail.com>
2023-04-24 14:08:51 -0700

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main -  program that prints its name, followed by a new line
+ * @argc: int
+ * @argv: array containing string
+ * Return: 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+(void)argc;
+}
